4f3cfd97a77a7138311da7a79b4f3f1a8fa0144f
Anar <anar.sol.dev@gmail.com>
2025-12-15 19:20:17 +0100

    add toggle button

--- tests/Navigation.test.ts
+++ tests/Navigation.test.ts
@@ -11,6 +11,8 @@ let items = [
     { label: "Contact", href: "/contact/" },
 ] as const;
 
+let navigation: HTMLElement | null;
+let list: HTMLElement | null;
 
 beforeEach(async () => {
     const container = await AstroContainer.create();
@@ -20,7 +22,10 @@ beforeEach(async () => {
             items,
         }
     });
+
     document.body.innerHTML = componentHTML;
+    navigation = document.querySelector("nav");
+    list = navigation?.querySelector("ul") || null;
 });
 
 test("There is exactly one nav element", () => {
@@ -28,34 +33,25 @@ test("There is exactly one nav element", () => {
 });
 
 test("aria-label is rendered correctly", () => {
-    const navigation = document.querySelector("nav");
     expect(navigation?.getAttribute("aria-label")).toBe(ariaLabel);
 });
 
 test("nav contains exactly one ul element", () => {
-    const navigation = document.querySelector("nav");
     expect(navigation?.querySelectorAll("ul")).toHaveLength(1);
 });
 
 test("ul element contains the good number of li", () => {
-    const navigation = document.querySelector("nav");
-    const list = navigation?.querySelector("ul");
     const listItems = list?.querySelectorAll("li");
     expect(listItems).toHaveLength(items.length);
 });
 
 test("Each list item contains exactly one link", () => {
-    const navigation = document.querySelector("nav");
-    const list = navigation?.querySelector("ul");
-    const listItems = list?.querySelectorAll("li");
-    listItems?.forEach((item) => expect(item?.querySelectorAll("a")).toHaveLength(1));
+    list?.querySelectorAll("li")
+        .forEach((item) => expect(item?.querySelectorAll("a")).toHaveLength(1));
 });
 
 test("Link text and href are correct", () => {
-    const navigation = document.querySelector("nav");
-    const list = navigation?.querySelector("ul");
-    const listItems = list?.querySelectorAll("li");
-    listItems?.forEach((item, index) => {
+    list?.querySelectorAll("li").forEach((item, index) => {
         const link = item?.querySelector("a")
         expect(link?.textContent.trim()).toBe(items[index].label);
         expect(link?.getAttribute("href")).toBe(items[index].href);
@@ -63,10 +59,32 @@ test("Link text and href are correct", () => {
 });
 
 test("aria-current is applied correctly", () => {
-    const navigation = document.querySelector("nav");
-    const list = navigation?.querySelector("ul");
     const currentLinks = list?.querySelectorAll('a[aria-current="page"]');
     expect(currentLinks).toHaveLength(1);
     expect(currentLinks?.[0].textContent.trim()).toBe(items[0].label);
     expect(currentLinks?.[0].getAttribute("href")).toBe(items[0].href);
+});
+
+test("There is exactly one button inside `<nav>`", () => {
+    expect(navigation?.querySelectorAll("button")).toHaveLength(1);
+});
+
+test("The button type is `button`", () => {
+    expect(navigation?.querySelector("button")?.getAttribute("type")).toBe("button");
+});
+
+test("The button is associated to the item list by aria-controls", () => {
+    const listID = list?.id;
+    expect(listID).toBeTruthy();
+    expect(navigation?.querySelector("button")?.getAttribute("aria-controls"))
+        .toBe(listID);
+});
+
+test("The toggle button is expanded by default (aria-expanded=true)", () => {
+    expect(navigation?.querySelector("button")?.getAttribute("aria-expanded"))
+        .toBe("true");
+});
+
+test("The item list is not hidden by default", () => {
+    expect(list?.hasAttribute("hidden")).toBeFalsy();
 });
